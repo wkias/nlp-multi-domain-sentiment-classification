b36755c4907e3e9778b8aea7acfc4a05abec4312
wkias <willicen@qq.com>
2020-10-17 18:43:12 +0800

    修bug

--- start.sh
+++ start.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 domains=('books' 'dvd' 'electronics' 'kitchen')
-for src_domain in ${domains[@]};
+for domain in ${domains[@]};
 do
-	for tar_domain in  ${domains[@]};
-	do
-		if [ $src_domain != $tar_domain ];
-		then
-            # echo "\n\n$src_domain\n" >> ./logs
-			# python code/main.py --pred_domain $src_domain | tee -a ./logs
-			python code/main.py --pred_domain $src_domain
-		fi
-	done
+		python code/main.py --pred_domain $domain
 done
 
